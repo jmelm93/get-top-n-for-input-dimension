85e4d053b0e2817da3f97059b960cd585a954f4f
Jason Melman <jason.melman93@gmail.com>
2022-12-05 17:37:07 -0800

    few updates

--- get_top_n.py
+++ get_top_n.py
@@ -1,29 +1,26 @@
 import pandas as pd 
 
-def get_top_n(df, grouped_dimension_list, analysis_dimension, metric, n):
+def get_top_n(df, metric, n):
     """get top n of dimension by metric"""
-    
-    # group by "grouped_dimension_list" and sum by "metric"
-    df = df.groupby(grouped_dimension_list)[metric].sum()
-    
-    # get top N for each "analysis_dimension" and reset index to clean up
-    return df.groupby(level=analysis_dimension, group_keys=False).nlargest(n).reset_index()
+    return df.sort_values(metric, ascending=False).head(n)
 
-def main(df, grouped_dimension_list, analysis_dimension, metric, n):
-    # get top 10 keywords by traffic
-    top_keywords_per_url = get_top_n(df, grouped_dimension_list, analysis_dimension, metric, n)
+def main(df, analysis_dimension, metric, n):
+    # Create empty df to hold final output
+    final = pd.DataFrame(columns=df.columns)
+    
+    # Get list of unique values in analysis_dimension
+    dimension_list = df[analysis_dimension].drop_duplicates().values.tolist()
+    
+    for dim in dimension_list:
+        # filter df by dim and get top n
+        top_n = get_top_n(df[df[analysis_dimension] == dim], metric, n)
+        # concat to final df
+        final = pd.concat([final, top_n])
 
     # merge top_keywords_per_url with df to bring extra columns
-    return top_keywords_per_url.merge(df, on=['URL', 'Keyword'], how='left')
+    return final
 
 if __name__ == '__main__':
-    df = pd.read_csv('INPUT_FILE.csv')
-
-    # filter df to Traffic > 0
-    df = df[df['Traffic'] > 10]
-    df = df[df['Search Volume'] > 100]
-    
-    output = main(df, ['URL', 'Keyword'], 'URL', 'Traffic', 3)
-
-    # write to csv
+    df = pd.read_csv('gsc.csv')
+    output = main(df, 'URL', 'Traffic', 6)
     output.to_csv('top-values-for-dimension-by-metric.csv', index=False)
